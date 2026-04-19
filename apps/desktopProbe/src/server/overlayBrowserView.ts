9038647301fd310f1b9e9550959daacfe180d033
jacobaguon-blip <jacob.aguon@conductorone.com>
2026-04-18 21:44:44 -0400

    update: apps/desktopProbe/src/server/overlayBrowserView.ts

--- apps/desktopProbe/src/server/overlayBrowserView.ts
+++ apps/desktopProbe/src/server/overlayBrowserView.ts
@@ -3,6 +3,23 @@ import { WebPageRuntimeData } from '@first2apply/core';
 import { BrowserWindow, WebContentsView } from 'electron';
 
 import { consumeRuntimeData } from './browserHelpers';
+import { logger } from './logger';
+
+function withTimeout<T>(promise: Promise<T>, ms: number, label: string): Promise<T> {
+  return new Promise<T>((resolve, reject) => {
+    const timer = setTimeout(() => reject(new Error(`${label} timed out after ${ms}ms`)), ms);
+    promise.then(
+      (value) => {
+        clearTimeout(timer);
+        resolve(value);
+      },
+      (err) => {
+        clearTimeout(timer);
+        reject(err);
+      },
+    );
+  });
+}
 
 /**
  * Class used to render a WebContentsView on top of the main window
